0e54b8b5bef3a625d64ea05a9ca21a010e5aef24
pedro Camargo <c@margo.co>
2019-10-11 16:13:46 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
-sudo apt-get install docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 mkdir data
 curl http://download.geofabrik.de/europe/albania-latest.osm.pbf -o ./data/great-britain-latest.osm.pbf 
 sudo docker-compose up
