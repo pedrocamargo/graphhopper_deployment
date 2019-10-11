c6cdaa0a48f73dfb090a0043d6cab3e4ab1cfe02
pedro Camargo <c@margo.co>
2019-10-11 15:53:19 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-curl https://download.geofabrik.de/europe/great-britain-latest.osm.pbf --output ./data/great-britain-latest.osm.pbf 
+mkdir data
+curl https://download.geofabrik.de/europe/great-britain-latest.osm.pbf -o ./data/great-britain-latest.osm.pbf 
 sudo docker-compose up
